2cf3f2840c9bc757a80f465259c68d844e6b6810
LauraRamirez1 <laura.ramirez1@gmail.com>
2019-02-11 12:40:34 -0300

    Proyecto actualizado con test

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,62 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset, entex1) => {
+    let ascii1 =0;
+    let ascii2 =0;
+    let result ="";
+    let entext= entex1.toUpperCase();
+ 
+    for (let i = 0; i < entext.length; i++) 
+    {
+        ascii1= entext.charCodeAt(i);
+        
+        if(ascii1 >=32 && ascii1 <=  47 || ascii1>= 58 && ascii1<= 64 ||ascii1>=91 && ascii1 <=96 ||ascii1>=123 && ascii1 <=163 ||ascii1>=165 ) 
+        {
+            ascii2=ascii1
+        }
+        else if(ascii1 >=48 && ascii1 <=  57)
+        {
+            ascii2= (ascii1- 48 + parseInt(offset))%10 + 48
+        }
+        else
+        {
+             ascii2= (ascii1- 65 + parseInt(offset))%26 + 65
+        }
+        
+        result += String.fromCharCode(ascii2);
+    }
+
+    return result;
+
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  
+  decode: (offset, entex1) => {
+    let ascii1 =0;
+    let ascii2 =0;
+    let result ="";
+    let entext= entex1.toUpperCase();
+ 
+    for (let i = 0; i < entext.length; i++) 
+    {
+        ascii1= entext.charCodeAt(i);
+        
+        if(ascii1 >=32 && ascii1 <=  47 || ascii1>= 58 && ascii1<= 64 ||ascii1>=91 && ascii1 <=96 ||ascii1>=123 && ascii1 <=163 ||ascii1>=165 ) 
+        {
+            ascii2=ascii1
+        }
+        else if(ascii1 >=48 && ascii1 <=  57)
+        {
+            ascii2= (ascii1- 57 - parseInt(offset))%10 + 57
+        }
+        else
+        {
+             ascii2= (ascii1- 90 - parseInt(offset))%26 + 90
+        }
+        
+        result += String.fromCharCode(ascii2);
+    }
+
+    return result;
+
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-/* Acá va tu código */
+
+const codebutton = document.getElementById("Code");
+codebutton.addEventListener('click', () => {
+    
+    let entex1 = document.getElementById("entex1").value;
+    let offset = document.getElementById("offset").value;
+      
+    if(parseInt(offset)<1){ 
+
+         document.getElementById("error").innerHTML =  "Debe ingresar un valor positivo de movimientos";
+    } else {
+        
+       document.getElementById("result1").innerHTML= cipher.encode(offset,entex1);   
+
+    }
+})
+
+const decodebutton = document.getElementById("Decode");
+decodebutton.addEventListener('click', () => {
+    
+    let entex1 = document.getElementById("entex1").value;
+    let offset = document.getElementById("offset").value;
+    
+    if(parseInt(offset)<1){ 
+        
+        document.getElementById("error").innerHTML =  "Debe ingresar un valor positivo de movimientos";
+    } else {
+        document.getElementById("result1").innerHTML= cipher.decode(offset,entex1);  
+    }
+})
+
+/*function startc2(){
+   
+    document.getElementById("Start").style.display = "none";
+    document.getElementById("Ingreso").style.display = "block";
+
+}
+function startc3(){
+
+    document.getElementById("Ingreso").style.display = "none";
+    document.getElementById("Datos").style.display = "block";
+
+    let name1 = document.getElementById("name").value;
+    
+
+    let email1 = document.getElementById("email").value;
+   
+}*/
